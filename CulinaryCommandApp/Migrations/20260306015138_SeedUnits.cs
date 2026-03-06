5bb11996d977fdb319a675f637ed94b85f7cadcc
Kevin Tran <kevtran04@gmail.com>
2026-03-05 21:34:31 -0600

    fix: github copilot suggestions

--- CulinaryCommandApp/Migrations/20260306015138_SeedUnits.cs
+++ CulinaryCommandApp/Migrations/20260306015138_SeedUnits.cs
@@ -10,40 +10,39 @@ namespace CulinaryCommand.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            // Negative IDs are reserved for seeded/system units so they never
+            // collide with user-created units (which use the positive identity sequence).
             migrationBuilder.InsertData(
                 table: "Units",
-                columns: new[] { "Name", "Abbreviation", "ConversionFactor" },
+                columns: new[] { "Id", "Name", "Abbreviation", "ConversionFactor" },
                 values: new object[,]
                 {
-                    { "Percent",     "%",    1m        },
-                    { "Each",        "ea",   1m        },
-                    { "Grams",       "g",    1m        },
-                    { "Kilograms",   "kg",   1000m     },
-                    { "Ounces",      "oz",   28.3495m  },
-                    { "Pounds",      "lb",   453.592m  },
-                    { "Milliliters", "mL",   1m        },
-                    { "Liters",      "L",    1000m     },
-                    { "Teaspoon",    "tsp",  4.92892m  },
-                    { "Tablespoon",  "tbsp", 14.7868m  },
-                    { "Cup",         "cup",  236.588m  },
-                    { "Quart",       "qt",   946.353m  },
-                    { "Gallon",      "gal",  3785.41m  },
-                    { "Serving",     "srv",  1m        },
+                    { -1,  "Percent",     "%",    1m        },
+                    { -2,  "Each",        "ea",   1m        },
+                    { -3,  "Grams",       "g",    1m        },
+                    { -4,  "Kilograms",   "kg",   1000m     },
+                    { -5,  "Ounces",      "oz",   28.3495m  },
+                    { -6,  "Pounds",      "lb",   453.592m  },
+                    { -7,  "Milliliters", "mL",   1m        },
+                    { -8,  "Liters",      "L",    1000m     },
+                    { -9,  "Teaspoon",    "tsp",  4.92892m  },
+                    { -10, "Tablespoon",  "tbsp", 14.7868m  },
+                    { -11, "Cup",         "cup",  236.588m  },
+                    { -12, "Quart",       "qt",   946.353m  },
+                    { -13, "Gallon",      "gal",  3785.41m  },
+                    { -14, "Serving",     "srv",  1m        },
                 });
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            // Delete by PK (Id) so we only remove the exact seeded rows and
+            // never accidentally touch user-created units that share a name.
             migrationBuilder.DeleteData(
                 table: "Units",
-                keyColumn: "Name",
-                keyValues: new object[]
-                {
-                    "Percent", "Each", "Grams", "Kilograms", "Ounces", "Pounds",
-                    "Milliliters", "Liters", "Teaspoon", "Tablespoon", "Cup",
-                    "Quart", "Gallon", "Serving"
-                });
+                keyColumn: "Id",
+                keyValues: new object[] { -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14 });
         }
     }
 }
